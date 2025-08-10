3dfb2b7f116b6552651a688008869264507bcc2c
DongGyun <wooheuy5752@gmail.com>
2025-08-10 20:05:56 +0900

    feat: 라우터 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { AppRouter } from './router/AppRouter'
+
 import styles from './App.module.css'
 
 function App() {
   return (
     <div className={styles.app}>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <AppRouter />
+      </div>
     </div>
   )
 }
