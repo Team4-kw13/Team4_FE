b0cae5c67b6c1312c280c25377935db944e59fe1
DongGyun <wooheuy5752@gmail.com>
2025-08-10 17:31:42 +0900

    chore: vite-plugin-svgr 패키지 설치

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 import react from '@vitejs/plugin-react'
 
@@ -8,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
