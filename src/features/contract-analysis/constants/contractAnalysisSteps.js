8b47b2cd7202dc891238694bf0a7f0f9f52d8a6a
DongGyun <wooheuy5752@gmail.com>
2025-08-10 22:52:17 +0900

    feat: ContractAnalysisStepList 구현

---
+++ src/features/contract-analysis/constants/contractAnalysisSteps.js
@@ -0,0 +1,10 @@
+export const contractAnalysisSteps = [
+  { title: '계약서를 업로드 하고', description: 'PDF나 이미지 파일을 선택해서 업로드해주세요' },
+  { title: '번역된 계약서가 나오면', description: 'AI가 계약서를 인식하고 번역본을 보여드려요' },
+  { title: '주요·주의 조항을 살펴보고', description: '하이라이트된 중요한 부분만 쏙쏙 확인해요' },
+  {
+    title: '쉬운 풀이+번역으로 이해하면',
+    description: '계약서 속 어려운 법률 조항과 조건을\r\n알기 쉬운 설명과 본인 언어로 안내해드려요',
+  },
+  { title: '끝' },
+]
