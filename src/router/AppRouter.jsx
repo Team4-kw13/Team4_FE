6de17a8813274e471cf372b321d56ed4286e24b3
HwangJiYoung <start014stop@naver.com>
2025-08-12 14:28:08 +0900

    feat: 마이페이지 라우팅

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { ContractAnalysis } from '../features/contract-analysis/pages/ContractAnalysis'
+import { MyPage } from '../features/mypage/pages/MyPage'
 
 import { ROUTES } from './routes.constant'
 
 const routes = [
   { path: ROUTES.HOME, element: <div>home</div> },
   { path: ROUTES.CONTRACT_ANALYSIS, element: <ContractAnalysis /> },
+  { path: ROUTES.MYPAGE, element: <MyPage /> },
 ]
 
 export const AppRouter = () => {
