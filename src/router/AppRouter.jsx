35002e4856479e0ed8f21b2464aa381cf9052c4c
HwangJiYoung <start014stop@naver.com>
2025-08-13 16:43:37 +0900

    feat: 리스트1 라우팅 추가

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { ContractAnalysis } from '../features/contract-analysis/pages/ContractAnalysis'
+import { ListSite } from '../features/list-site/pages/ListSite'
 import { MyPage } from '../features/mypage/pages/MyPage'
 
 import { ROUTES } from './routes.constant'
@@ -9,6 +10,7 @@ const routes = [
   { path: ROUTES.HOME, element: <div>home</div> },
   { path: ROUTES.CONTRACT_ANALYSIS, element: <ContractAnalysis /> },
   { path: ROUTES.MYPAGE, element: <MyPage /> },
+  { path: ROUTES.LIST_SITE, element: <ListSite /> },
 ]
 
 export const AppRouter = () => {
