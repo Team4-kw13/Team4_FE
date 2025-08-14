8828bc15b93c2575cd277bd7556938becb73bb36
HwangJiYoung <start014stop@naver.com>
2025-08-14 14:15:58 +0900

    feat: 리스트2 메인

---
+++ src/features/list-lawyer/pages/ListLawyer.jsx
@@ -0,0 +1,18 @@
+import { BackButton } from '@/components/BackButton/BackButton'
+import { ListLawyerHeader } from '@/features/list-lawyer/components/ListLawyerHeader'
+import { ListLawyerList } from '@/features/list-lawyer/components/ListLawyerList'
+
+import styles from './ListLawyer.module.css'
+
+export const ListLawyer = () => {
+  return (
+    <div className={styles['container']}>
+      <nav className={styles['back-button']}>
+        <BackButton />
+      </nav>
+
+      <ListLawyerHeader />
+      <ListLawyerList />
+    </div>
+  )
+}
