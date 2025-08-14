fea3d1390e50b4cb5698db458452727881fe0531
HwangJiYoung <start014stop@naver.com>
2025-08-14 14:15:24 +0900

    feat: 리스트2 상단 헤더

---
+++ src/features/list-lawyer/components/ListLawyerHeader.jsx
@@ -0,0 +1,23 @@
+import styles from './ListLawyerHeader.module.css'
+
+/**
+ * 변호사 리스트 상단 헤더
+ * @param {object} props
+ * @param {number} [props.count]  - 변호사 수(뱃지에 표시)
+ * @param {() => void} [props.onMore] - 우측 "더보기" 클릭 핸들러
+ * @param {string} [props.moreHref]   - 링크 사용 시 href 전달(있으면 <a>, 없으면 <button>)
+ */
+export const ListLawyerHeader = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <h3 className={styles.title}>변호사</h3>
+
+        <div className={styles.metaRow}>
+          <span className={styles.metaLabel}>AD LAWYERS</span>
+          <span className={styles.adBadge}>광고</span>
+        </div>
+      </div>
+    </div>
+  )
+}
