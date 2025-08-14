74904513b9505eb09bb68ebcfdf8185cf72397fc
DongGyun <wooheuy5752@gmail.com>
2025-08-14 15:04:39 +0900

    feat: ContractAnalysis 접근 시 스텝 초기화

--- src/features/contract-analysis/pages/ContractAnalysis.jsx
+++ src/features/contract-analysis/pages/ContractAnalysis.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+
 import { BackButton } from '@/components/BackButton/BackButton'
 import { useStep } from '@/stores/useStep'
 
@@ -8,8 +10,23 @@ import { UploadedImagesProvider } from '../stores/UploadedImagesProvider'
 
 import styles from './ContractAnalysis.module.css'
 
+/**
+ * 계약서 분석 플로우의 루트 컨테이너
+ *
+ * - 페이지 진입 시 `reset()`으로 스텝 상태를 초기화 (항상 1단계부터 시작)
+ * - 업로드된 이미지 전역 상태를 `UploadedImagesProvider`로 감쌈
+ *
+ * @returns {JSX.Element}
+ */
+
 export const ContractAnalysis = () => {
   const { currentStep, goToNextStep, goToPrevStep, reset } = useStep(3)
+
+  useEffect(() => {
+    if (currentStep !== 1) {
+      reset()
+    }
+  }, [currentStep, reset])
 
   return (
     <UploadedImagesProvider>
