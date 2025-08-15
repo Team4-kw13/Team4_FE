deb2f9d0f218aa5148663d6dc893f8ae51323665
HwangJiYoung <start014stop@naver.com>
2025-08-15 14:18:06 +0900

    feat: 홈화면 메인

---
+++ src/features/home/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import { HomePageAnalysis } from '../components/HomePageAnalysis'
+import { HomePageClick } from '../components/HomePageClick'
+import { HomePageHeader } from '../components/HomePageHeader'
+import { HomePageLegal } from '../components/HomePageLegal'
+
+import styles from './HomePage.module.css'
+
+export const HomePage = () => {
+  return (
+    <div className={styles['container']}>
+      <HomePageHeader />
+
+      <HomePageClick />
+
+      <div className={styles.cardsRow}>
+        <HomePageAnalysis />
+        <HomePageLegal />
+      </div>
+    </div>
+  )
+}
