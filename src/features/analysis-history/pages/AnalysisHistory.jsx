499948d29d041c9f2f884a333ba6207dfef3628e
DongGyun <wooheuy5752@gmail.com>
2025-08-16 14:47:06 +0900

    feat: 분석 기록 페이지 뒤로가기 버튼 적용

--- src/features/analysis-history/pages/AnalysisHistory.jsx
+++ src/features/analysis-history/pages/AnalysisHistory.jsx
@@ -1,13 +1,14 @@
 import { useRef } from 'react'
 import cx from 'classnames'
 
+import { BackButton } from '@/components/BackButton/BackButton'
 import { ScrollToTopButton } from '@/components/ScrollToTopButton/ScrollToTopButton'
-import { ContractAnalysisSummary } from '@/features/contract-analysis/containers/ContractAnalysisSummary'
 import { useScrollSnap } from '@/features/contract-analysis/hooks/useScrollSnap'
 import { DocumentAnalysisProvider } from '@/stores/DocumentAnalysisProvider'
 import { useStep } from '@/stores/useStep'
 
 import { AnalysisHistoryHighlight } from '../containers/AnalysisHistoryHighlight'
+import { AnalysisHistorySummary } from '../containers/AnalysisHistorySummary'
 import { AnalysisHistoryTranslate } from '../containers/AnalysisHistoryTranslate'
 
 import styles from './AnalysisHistory.module.css'
@@ -22,38 +23,44 @@ export const AnalysisHistory = () => {
 
   return (
     <DocumentAnalysisProvider>
-      <div className={styles['background']} />
-      <div ref={carouselRef} className={styles['carousel-container']}>
-        <section
-          ref={slideRefs[0]}
-          className={cx(styles['carousel-item'], styles['snap-item'])}
-          data-index={1}
-          aria-label={'번역 섹션'}
-        >
-          <AnalysisHistoryTranslate />
-        </section>
+      <div className={styles['container']}>
+        <div className={styles['background']} />
+        <nav className={styles['back-button']}>
+          <BackButton />
+        </nav>
 
-        <section
-          ref={slideRefs[1]}
-          className={cx(styles['carousel-item'], styles['snap-item'])}
-          data-index={2}
-          aria-label={'하이라이트 섹션'}
-        >
-          <AnalysisHistoryHighlight />
-        </section>
+        <div ref={carouselRef} className={styles['carousel-container']}>
+          <section
+            ref={slideRefs[0]}
+            className={cx(styles['carousel-item'], styles['snap-item'])}
+            data-index={1}
+            aria-label={'번역 섹션'}
+          >
+            <AnalysisHistoryTranslate />
+          </section>
 
-        <section
-          ref={slideRefs[2]}
-          className={styles['carousel-item']}
-          data-index={3}
-          aria-label={'요약 섹션'}
-        >
-          <ContractAnalysisSummary containerRef={carouselRef} />
+          <section
+            ref={slideRefs[1]}
+            className={cx(styles['carousel-item'], styles['snap-item'])}
+            data-index={2}
+            aria-label={'하이라이트 섹션'}
+          >
+            <AnalysisHistoryHighlight />
+          </section>
 
-          <div className={styles['scroll-button']}>
-            <ScrollToTopButton targetRef={carouselRef} />
-          </div>
-        </section>
+          <section
+            ref={slideRefs[2]}
+            className={styles['carousel-item']}
+            data-index={3}
+            aria-label={'요약 섹션'}
+          >
+            <AnalysisHistorySummary containerRef={carouselRef} />
+
+            <div className={styles['scroll-button']}>
+              <ScrollToTopButton targetRef={carouselRef} />
+            </div>
+          </section>
+        </div>
       </div>
     </DocumentAnalysisProvider>
   )
