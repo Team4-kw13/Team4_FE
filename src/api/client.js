571d399bb9f9f31219d4b282ca077b43a5735583
DongGyun <wooheuy5752@gmail.com>
2025-08-23 16:01:46 +0900

    fix: 테스트용 요청 제거

--- src/api/client.js
+++ src/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_DOMAIN,
   withCredentials: true,
-  headers: { 'Content-Type': 'application/json', Authorization: import.meta.env.VITE_SERVER_KT },
+  headers: { Authorization: import.meta.env.VITE_SERVER_KT },
 })
 
 instance.interceptors.request.use(
