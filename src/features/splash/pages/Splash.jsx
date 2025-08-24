b2b41fb27c981aeec60511caa8c1724d56bf1dcc
DongGyun <wooheuy5752@gmail.com>
2025-08-24 11:58:10 +0900

    design: 스플래시 이미지 위치 변경

--- src/features/splash/pages/Splash.jsx
+++ src/features/splash/pages/Splash.jsx
@@ -4,21 +4,26 @@ import styles from './Splash.module.css'
 
 export function Splash() {
   return (
-    <section className={styles.container}>
-      <h1 className={styles.brand}>한집말이</h1>
-      <p className={styles.title}>
-        당신의 집,
-        <br />
-        <span className={styles.em}>당신</span>의 말로
-      </p>
-
-      <div className={styles['icon-container']}>
-        <Icon name='note' width={251} height={260} className={styles['icon']} />
-        <Icon name='house' width={366} height={366} className={styles['icon']} />
-        <Icon name='star' width={246} className={styles['icon']} />
-      </div>
+    <div className={styles['container']}>
       <div className={styles['circle']} />
-    </section>
+
+      <section className={styles['content']}>
+        <header className={styles['header']}>
+          <h1 className={styles.brand}>한집말이</h1>
+          <p className={styles.title}>
+            당신의 집,
+            <br />
+            <span className={styles.em}>당신</span>의 말로
+          </p>
+        </header>
+
+        <div className={styles['icon-container']}>
+          <Icon name='note' width={255} height={265} className={styles['icon']} />
+          <Icon name='house' width={366} height={366} className={styles['icon']} />
+          <Icon name='star' width={246} className={styles['icon']} />
+        </div>
+      </section>
+    </div>
   )
 }
 
