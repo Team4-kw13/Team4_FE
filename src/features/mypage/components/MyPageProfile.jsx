fd391a74e6f2a186eb9466ed2cb755f6dc8970a5
HwangJiYoung <start014stop@naver.com>
2025-08-25 16:36:15 +0900

    fix: 마이페이지 연결 수정

--- src/features/mypage/components/MyPageProfile.jsx
+++ src/features/mypage/components/MyPageProfile.jsx
@@ -1,22 +1,7 @@
-import { useEffect } from 'react'
-
-import { useMypage } from '../hooks/useMypage'
-
 import styles from './MyPageProfile.module.css'
 
 /** 마이페이지 상단 프로필 */
-export const MyPageProfile = () => {
-  const { data, loading, error, refetch } = useMypage()
-
-  useEffect(() => {
-    refetch()
-  }, [refetch])
-
-  if (loading) return null
-  if (error) return <p>오류발생</p>
-
-  const nickname = data?.nickname ?? '사용자'
-
+export const MyPageProfile = ({ nickname }) => {
   return (
     <header className={styles.container}>
       <div className={styles.avatar}></div>
