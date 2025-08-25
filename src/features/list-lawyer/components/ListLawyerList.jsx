1595e4e6bd56165375abf7ca98ce54bfc6691573
DongGyun <wooheuy5752@gmail.com>
2025-08-25 18:54:51 +0900

    fix: 변호사 더미데이터 수정

--- src/features/list-lawyer/components/ListLawyerList.jsx
+++ src/features/list-lawyer/components/ListLawyerList.jsx
@@ -3,28 +3,44 @@ import ListLawyerItem from './ListLawyerItem'
 import styles from './ListLawyerList.module.css'
 
 export const ListLawyerList = () => {
-  const base = [
+  const lawyers = [
     {
+      id: 1,
       title: '강영현',
       lawFirm: '법무법인 계란',
       description: '[4대 보험, 법인 설립] 계약서 작성, 계약 해지',
     },
     {
-      title: '강영현',
+      id: 2,
+      title: '박경민',
       lawFirm: '법무법인 계란',
       description: '[4대 보험, 법인 설립] 계약서 작성, 계약 해지',
     },
     {
-      title: '강영현',
+      id: 3,
+      title: '김현진',
+      lawFirm: '법무법인 계란',
+      description: '[4대 보험, 법인 설립] 계약서 작성, 계약 해지',
+    },
+    {
+      id: 4,
+      title: '정서진',
+      lawFirm: '법무법인 계란',
+      description: '[4대 보험, 법인 설립] 계약서 작성, 계약 해지',
+    },
+    {
+      id: 5,
+      title: '고은빈',
+      lawFirm: '법무법인 계란',
+      description: '[4대 보험, 법인 설립] 계약서 작성, 계약 해지',
+    },
+    {
+      id: 6,
+      title: '이효원',
       lawFirm: '법무법인 계란',
       description: '[4대 보험, 법인 설립] 계약서 작성, 계약 해지',
     },
   ]
-
-  const lawyers = Array.from({ length: 12 }, (_, i) => ({
-    ...base[i % base.length],
-    id: i + 1,
-  }))
 
   return (
     <div className={styles.list}>
